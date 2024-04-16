40485babf7addec20988567da242357ba70ecd68
james60485 <jameslatteier@runbox.com>
2024-04-15 17:46:56 -0700

    Changed Travaux gallery to columns, italic titles for all pages.

---
+++ wp-admin/edit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirection...</title>
+		<meta http-equiv="refresh" content="0;url=https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D5521&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D5521&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D5521&reauth=1">https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D5521&reauth=1</a></p>
+	</body>
+</html>
