4dde74e5d3221514388aa26ceefe586f4da85e29
james60485 <jameslatteier@runbox.com>
2024-04-18 15:06:15 -0700

    Further changes to typography.

--- wp-admin/edit.php
+++ wp-admin/edit.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirection...</title>
-		<meta http-equiv="refresh" content="0;url=https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D6616&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D6981&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D6616&reauth=1";
+			window.location = "https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D6981&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D6616&reauth=1">https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D6616&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D6981&reauth=1">https://james60485.github.io/wp-login.php?redirect_to=http%3A%2F%2Fhtc-wp.ddev.site%3A8000%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D6981&reauth=1</a></p>
 	</body>
 </html>
